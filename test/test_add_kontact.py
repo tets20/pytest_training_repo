37d5ae610efa0c760018b1c147a73248fed58252
test_20 <y.s.bubko@gmail.com>
2018-04-18 06:27:48 +0300

    Выделили вспомогательный класс SessionHelper

--- test/test_add_kontact.py
+++ test/test_add_kontact.py
@@ -11,7 +11,7 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.create_contact(Kontact(firstname="Test_contact", middlename="Test_contact", lastname="Test_contact",
                                    nickname="TC", title="Test_user", company="Test_company", address="Test_country",
                                    home="Home_test", mobile="00-00-00", work="00-00-00", fax="00-00-00",
@@ -19,13 +19,13 @@ def test_add_contact(app):
                                    homepage="test.test.ru", byear="1901", ayear="1991", address2="Test_city",
                                    home2="Test_home", notes="Test_contact"))
 
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login( username="admin", password="secret")
+    app.session.login( username="admin", password="secret")
     app.create_contact(Kontact(firstname="", middlename="", lastname="", nickname="", title="", company="",
                                     address="", home="", mobile="", work="", fax="", email="", email2="", email3="",
                                    homepage="", byear="", ayear="", address2="", home2="", notes=""))
-    app.logout()
+    app.session.logout()
 
