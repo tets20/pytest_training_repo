8ef33385207c8350609c901c3752474fc4ec70de
test_20 <y.s.bubko@gmail.com>
2018-05-17 00:40:57 +0300

    Генерация тестовых данных в формате json

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 
+
+from data.groups import constant as Testdata
 from model.group import Group
-import random
-import string
 import pytest
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation +" "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-Testdata =[Group(name="", header="", footer="")] + [
-    Group(name=random_string("name",10), header=random_string("header",20), footer=random_string("footer",20))
-    for i in range(5)
-]
 
 @pytest.mark.parametrize("group",Testdata, ids = [repr(x) for x in Testdata])
 def test_add_group(app,group):
